9fde306c7a3056f85cc914da04a93611d4e7b911
DonnCycu <kglogowski99@gmail.com>
2023-01-19 19:58:26 +0100

    e

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="movie">
+    <nav class="movie__nav">
+      <a class="logo">
+        <span>Awesome</span><br/>
+        <span>Movie</span>
+      </a>
+      <ul>
+        <li>Home</li>
+        <li>Movies</li>
+      </ul>
+    </nav>
+    <div class="movie__list">
+      <h2>Watch your favourite movie!</h2>
+      <MoviesList/>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MoviesList from "@/components/MoviesList";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MoviesList
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+body {
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color:purple;
+}
+.movie {
+  background: linear-gradient(180deg, #010113 0%, #0E0E31 100%);
+  height:100%;
+  width:100vw;
+  color: #e1e1e1;
+  &__nav {
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    position:fixed;
+    top:0;
+    left:0;
+    right:0;
+    padding:1em 2em;
+    margin:0 auto;
+    width:80%;
+    z-index: 2;
+    .logo {
+      display: flex;
+      align-items:start;
+      justify-content: start;
+      flex-direction: column;
+      font-size:1.3rem;
+      line-height: 12px;
+      letter-spacing: .15em;
+    }
+    ul {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      list-style-type: none;
+      li {
+        margin-right:1em;
+        padding:.2em 1em;
+        text-decoration: none;
+        cursor:pointer;
+      }
+    }
+  }
+  &__list {
+    display:flex;
+    align-items: center;
+    justify-contect:center;
+    flex-direction: column;
+    margin:0 auto;
+    padding-top:10em;
+    width:60%;
+    h2 {
+      margin-bottom:4em;
+    }
+  }
 }
 </style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,58 +1,168 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="movies">
+     <div class="movies__box" v-for="movie in listOfMovies" :key="movie">
+       <div class="info">
+         <div class="info__title">
+           <span>{{ movie.movieTitle }}</span>
+           <span>{{ movie.movieFullTime }} minuty</span>
+         </div>
+         <button class="info__play" @click="() => open(movie.id)">
+           <img src="@/assets/play.png" style="width:20px;" alt="Play button"/>
+           <span>Obejrzyj film</span>
+         </button>
+       </div>
+       <div v-if="movie.isOpen" class="movie">
+           <video @play="start" @pause="middle" @ended="end" controls>
+             <source :src="movie.movieUrl" type="video/mp4">
+           </video>
+       </div>
+     </div>
   </div>
 </template>
 
 <script>
+import {reactive} from "vue";
+import movie1 from "@/assets/movies/movie1.mp4"
+import movie2 from "@/assets/movies/movie2.mp4"
+import movie3 from "@/assets/movies/movie3.mp4"
+import movie4 from "@/assets/movies/movie4.mp4"
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'MoviesList',
+  components:{},
+  setup(){
+    const movies = reactive([
+      movie1, movie2, movie3, movie4
+    ])
+    const listOfMovies = reactive([
+      {
+        id:0,
+        movieTitle:'W pustyni i w puszczy',
+        movieUrl:'',
+        movieFullTime:null,
+        isOpen:false
+      },
+      {
+        id:1,
+        movieTitle:'Tajemniczy smark na firanie',
+        movieUrl:'',
+        movieFullTime:null,
+        isOpen:false
+      },
+      {
+        id:2,
+        movieTitle:'Zabójcza opona',
+        movieUrl:'',
+        movieFullTime:null,
+        isOpen:false
+      },
+      {
+        id:3,
+        movieTitle:'Czeski film',
+        movieUrl:'',
+        movieFullTime:null,
+        isOpen:false
+      }
+    ]);
+
+    listOfMovies.map((item,index) => item.movieUrl = movies[index])
+
+    function open(index) {
+      const currentMovie = listOfMovies.find(movie => movie.id === index);
+      if(currentMovie.isOpen) {
+        currentMovie.isOpen = false;
+        return;
+      }
+      listOfMovies.forEach((movie, i) => {
+        movie.isOpen = i === index;
+      });
+    }
+    return{
+      listOfMovies,
+      open,movies
+    }
+  },
+  mounted() {
+    if (Array.isArray(this.$refs.video) && this.$refs.video.length > 0) {
+      this.$refs.video.forEach(video => {
+        video.addEventListener('play', this.start)
+        video.addEventListener('pause', this.middle)
+        video.addEventListener('ended', this.end)
+      })
+    }
+  },
+  methods: {
+    start() {
+      console.log('początek filmu')
+    },
+    middle() {
+      console.log('środek filmu')
+    },
+    end() {
+      console.log('koniec filmu')
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+<style scoped lang="scss">
+
+.movies {
+  display:flex;
+  align-items: center;
+  justify-contect:center;
+  flex-direction: column;
+  padding:0 1em;
+  width:100%;
+  &__box {
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin-bottom:2em;
+    width:100%;
+    height:100%;
+    background: rgba(196, 196, 196, 0.01);
+    box-shadow: inset 0px 39px 56px -36px rgba(255, 255, 255, 0.1), inset 0px 7px 11px -4px #FFFFFF, inset 0px -82px 68px -64px rgba(96, 68, 145, 0.1), inset 0px 98px 100px -48px rgba(202, 172, 255, 0.1), inset 0px 4px 18px rgba(154, 146, 210, 0.1), inset 0px 1px 40px rgba(227, 222, 255, 0.2);
+    backdrop-filter: blur(50px);
+    border-radius: 120px;
+  }
+  .info {
+    display:flex;
+    align-items: center;
+    justify-content: space-between;
+    padding:2em;
+    width:80%;
+    &__title {
+      display:flex;
+      align-items:start;
+      justify-content: center;
+      flex-direction: column;
+      :nth-child(1){
+        margin-bottom:.4em;
+        font-size:1.3rem;
+      }
+      :nth-child(2){
+        font-size:.8rem;
+      }
+    }
+    &__play {
+      display:flex ;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      background-color:transparent;
+      border:none;
+      cursor:pointer;
+      span {
+        margin-top:.4em;
+        color:white;
+      }
+    }
+   }
+  .movie, video {
+    width:100%;
+    height:100%;
+  }
 }
 </style>
