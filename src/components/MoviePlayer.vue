f91a17882d6710105e8efeaa93cb84145dff89d4
DonnCycu <kglogowski99@gmail.com>
2023-01-20 22:09:08 +0100

    finish task

---
+++ src/components/MoviePlayer.vue
@@ -0,0 +1,49 @@
+<template>
+  <video ref="videoPlayer" @loadedmetadata="loadData" @timeupdate="timeLogger" @play="start" @pause="middle"
+         @ended="end" controls>
+    <source :src="movie.filePath" type="video/mp4">
+  </video>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "MoviePlayer",
+  props: ['movie'],
+  setup() {
+    const videoPlayer = ref(null);
+    const duration = ref(0);
+    const halfDuration = ref(0);
+    const lastEventTime = ref(0);
+
+    function timeLogger() {
+      if ((Date.now() - lastEventTime.value) >= 1000) {
+        lastEventTime.value = Date.now();
+
+        const currentTime = Math.floor(videoPlayer.value.currentTime);
+        if (currentTime === 0) {
+          console.log("Poczatek");
+        }
+        if (halfDuration.value == currentTime) {
+          console.log("Polowa")
+        }
+        if (currentTime == duration.value) {
+          console.log("koniec");
+        }
+      }
+    }
+
+    function loadData() {
+      this.duration = Math.floor(videoPlayer.value.duration);
+      this.halfDuration = Math.floor(videoPlayer.value.duration / 2);
+    }
+
+    return {videoPlayer, duration, halfDuration, loadData, timeLogger, lastEventTime}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
